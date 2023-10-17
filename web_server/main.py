735857383346b9a4a1fb4ee664709c0626694e0f
jpm2000 <124071555+jpm2000@users.noreply.github.com>
2023-10-17 11:37:28 -0500

    I made the first web server

--- web_server/main.py
+++ web_server/main.py
@@ -1,4 +1,31 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return[1,2,3,4,5]
+
+@app.get('/contact')
+def get_list():
+    return{'name': 'Platzi'} 
+
+@app.get('/response', response_class=HTMLResponse)
+def get_list():
+    return"""
+    <html>
+            <head>
+                <title>Esta es una prueba de HTML con fastAPI</title>
+            </head>
+            <body>
+                <h1>Cree un servidor web</h1>
+            </body>
+            <p>Aca puedo escribir mas cosas</p>
+        </html>
+    """
+
 def run():
     store.get_categories()
 
